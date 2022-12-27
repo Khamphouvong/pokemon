bd72da36b04aa8c3d546bb7988a26fb302f4dde6
Khamphouvong.kcsv@gmail.com <Khamphouvong.kcsv@gmail.com>
2022-12-27 15:54:08 +0700

    migrate to customScrollView

--- lib/features/pokedex/presentation/screens/widgets/pokemon_list.dart
+++ lib/features/pokedex/presentation/screens/widgets/pokemon_list.dart
@@ -7,38 +7,56 @@ import 'package:pokemon/features/pokedex/presentation/screens/widgets/image_widg
 
 class PokemonList extends StatelessWidget {
   const PokemonList(
-      {super.key, required this.scrollController, required this.pokemonList});
+      {super.key,
+      required this.scrollController,
+      required this.pokemonList,
+      this.loading});
   final ScrollController scrollController;
   final List<Result> pokemonList;
+  final Widget? loading;
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        controller: scrollController
-          ..addListener(() {
-            double maxScroll = scrollController.position.maxScrollExtent;
-            double currentScroll = scrollController.position.pixels;
-            double delta = MediaQuery.of(context).size.height * 0.20;
-            if (maxScroll - currentScroll <= delta) {
-              context.read<PokedexCubit>().getPokedexMore(10, 0);
-            }
-          }),
-        padding: const EdgeInsets.all(8),
-        shrinkWrap: true,
-        itemCount: pokemonList.length,
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ImageWidget(index: index),
-                  Text(pokemonList[index].nameEntity.capitalize()),
-                  Text('${index + 1}'),
-                ],
-              ),
-              const Divider(),
-            ],
-          );
-        });
+    return CustomScrollView(
+      //add scroll controller when scroll max then read Pokedex Cubit
+      controller: scrollController
+        ..addListener(() {
+          double maxScroll = scrollController.position.maxScrollExtent;
+          double currentScroll = scrollController.position.pixels;
+          double delta = MediaQuery.of(context).size.height * 0.20;
+          if (maxScroll - currentScroll <= delta) {
+            context.read<PokedexCubit>().getPokedexMore(10, 0);
+          }
+        }),
+      slivers: [
+        const SliverAppBar(
+          pinned: true,
+        ),
+        SliverList(
+            delegate: SliverChildBuilderDelegate(
+          childCount: pokemonList.length,
+          (context, index) {
+            return Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ImageWidget(index: index),
+                    Text(
+                      pokemonList[index].nameEntity.capitalize(),
+                      textAlign: TextAlign.start,
+                    ),
+                    Text('${index + 1}'),
+                  ],
+                ),
+                const Divider(),
+              ],
+            );
+          },
+        )),
+
+        //if Scroll is max run loading widget
+        loading ?? const SliverToBoxAdapter(child: SizedBox())
+      ],
+    );
   }
 }
